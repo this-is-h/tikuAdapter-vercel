ded414fdb70ba1eb76ea8d4d6a1fc20ecd64e64f
this-is-h <thisish@foxmail.com>
2024-06-18 15:37:50 +0800

    test 2

--- api/search.go
+++ api/search.go
@@ -1,10 +1,24 @@
+// api/index.go
 package handler
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vercel/go-bridge/go/bridge"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.New()
+
+	// 定义你的路由
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello from Gin on Vercel!",
+		})
+	})
+
+	// 处理请求
+	bridge.NewHandlerFunc(router.ServeHTTP).ServeHTTP(w, r)
 }
