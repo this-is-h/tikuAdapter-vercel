c79af4d5272a5079e250b581e345f621e1184290
this-is-h <thisish@foxmail.com>
2024-06-18 16:40:00 +0800

    test6

--- api/search.go
+++ api/search.go
@@ -1,15 +1,9 @@
 package handler
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	router := gin.New()
-	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello from Gin!")
-	})
-	router.ServeHTTP(w, r)
+func Handler(c *gin.Context) {
+	c.String(http.StatusOK, "Hello from Gin!")
 }
